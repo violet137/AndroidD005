14e94e71444c0481c75df5421e04ee2dff3ac99f
Z_Dragon <thepathtosuccess1346@gmail.com>
2017-10-28 21:12:35 +0700

    Adding some packages.

--- Code/GA_FinalProjectHM/app/src/main/java/com/greenacademy/ga_finalprojecthm/MainActivity.java
+++ Code/GA_FinalProjectHM/app/src/main/java/com/greenacademy/ga_finalprojecthm/MainActivity.java
@@ -2,6 +2,7 @@ package com.greenacademy.ga_finalprojecthm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 

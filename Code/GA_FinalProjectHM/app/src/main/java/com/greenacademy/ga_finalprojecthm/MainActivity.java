329cea9d2603d7464024db0ef33fe4781bc97e6e
DESKTOP-S5R02RU\xuanson <xuansona13@gmail.com>
2017-10-31 20:19:19 +0700

    Revert "google+"
    This reverts commit b5fae8494a51174260d37e19a1d79be8345a98b3.

--- Code/GA_FinalProjectHM/app/src/main/java/com/greenacademy/ga_finalprojecthm/MainActivity.java
+++ Code/GA_FinalProjectHM/app/src/main/java/com/greenacademy/ga_finalprojecthm/MainActivity.java
@@ -2,15 +2,13 @@ package com.greenacademy.ga_finalprojecthm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
